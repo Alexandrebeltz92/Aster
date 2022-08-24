e6425d15f8debb59eada9d46d75d0923b035c83f
Alexandrebeltz92 <75671812+Alexandrebeltz92@users.noreply.github.com>
2022-08-24 22:01:55 +0200

    adding new cards

--- Aster/Scenes/Tarot/TarotCardModelView.swift
+++ Aster/Scenes/Tarot/TarotCardModelView.swift
@@ -17,7 +17,7 @@ extension CardFront  {
     class CardFrontModelView: ObservableObject {
         @Published var tarotCard = "0 The Fool"
 
-        let tarotCards = ["0 The Fool", "1 The Magician", "2 The High Priestess", "3 The Empress"]
+        let tarotCards = ["0 The Fool", "1 The Magician", "2 The High Priestess", "3 The Empress", "5 The Hierophant", "7 The Chariot", "9 The Hermit", "19 The Sun", "21 The World"]
 
         func getRandomCard() {
             tarotCard = tarotCards.randomElement() ?? "0_The_Fool"
