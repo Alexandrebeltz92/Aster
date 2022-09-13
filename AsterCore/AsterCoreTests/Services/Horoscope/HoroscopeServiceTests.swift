5312585fbce44f40387633cb6400ae6d49336526
Alexandrebeltz92 <75671812+Alexandrebeltz92@users.noreply.github.com>
2022-09-13 11:11:34 +0200

    add tests

---
+++ AsterCore/AsterCoreTests/Services/Horoscope/HoroscopeServiceTests.swift
@@ -0,0 +1,50 @@
+//
+//  HoroscopeServiceTests.swift
+//  AsterCoreTests
+//
+//  Created by Alexandre Graverol on 12/09/2022.
+//
+
+import XCTest
+@testable import AsterCore
+
+class HoroscopeServiceTests: XCTestCase {
+
+    // MARK: - Properties
+    var stubNetworkService: StubNetworkService!
+    var service: HoroscopeService!
+
+    override func setUp() {
+        super.setUp()
+
+        stubNetworkService = StubNetworkService()
+        service = HoroscopeService(networkService: stubNetworkService)
+    }
+
+    func test_GivenAResponse_WhenGetRecipe_ShouldReturnSameResponse() {
+        let response = HoroscopeResponse(sign: "Aries", text: "Hello", date: "25", type: "OK")
+        stubNetworkService.stubData = response
+
+        service.getHoroscope(for: "Aries", for: "Today") { (result: Result<HoroscopeResponse?, ServiceError>) in
+            switch result {
+            case .success(let value):
+                XCTAssertEqual(value?.sign, "Aries")
+            case .failure:
+                NSLog("Should have succeed")
+            }
+        }
+    }
+
+    func test_GivenABrokenResponse_WhenGetRecipe_ShouldReturnError() {
+        stubNetworkService.stubError = .unknown
+
+        service.getHoroscope(for: "Aries", for: "Today") { (result: Result<HoroscopeResponse?, ServiceError>) in
+            switch result {
+            case .success:
+                NSLog("Should have succeed")
+            case .failure(let error):
+                XCTAssertEqual(error.errorDescription, ServiceError.unknown.errorDescription)
+            }
+        }
+    }
+}
