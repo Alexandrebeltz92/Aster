1cd5f23e401aea398190d1aeaeb72d6a90dbfe0d
Alexandrebeltz92 <75671812+Alexandrebeltz92@users.noreply.github.com>
2022-10-03 18:08:14 +0200

    adding tests

--- AsterCore/Tests/Services/CoreData/CoreDataPersistenceCardTests.swift
+++ AsterCore/Tests/Services/CoreData/CoreDataPersistenceCardTests.swift
@@ -16,6 +16,7 @@ class CoreDataPersistenceCardTests: XCTestCase {
                          cards: [])
 
     let testCard = Card(name: "The Magicien", imageName: "1_The_Magicien", description: "", saved: false)
+    let testCard2 = Card(name: "The High Priestess", imageName: "2_The_High_Priestess", description: "", saved: false)
 
     var coreDataPersistence: CoreDataPersistence!
     var coreDataPersistenceCard: CoreDataPersistenceCard!
@@ -67,5 +68,17 @@ class CoreDataPersistenceCardTests: XCTestCase {
         coreDataPersistenceCard.delete(card: testCard, user: testUser)
 
         XCTAssertTrue(testUser.cards.count == 0)
+    }
+
+    func test_update_user_cards() {
+        coreDataPersistence.persist(user: testUser)
+        coreDataPersistenceCard.persist(card: testCard, user: testUser)
+        coreDataPersistenceCard.persist(card: testCard2, user: testUser)
+
+        coreDataPersistenceCard.update(card: testCard2, for: testUser)
+        let cards = coreDataPersistenceCard.getCards(for: testUser)
+
+        XCTAssertNotNil(cards)
+        XCTAssertTrue(testUser.cards.count == 2)
     }
 }
